7bed554dc037beb1972bd31d0d6095d95ddacd83
Raphael Muniz <raphaeldecm@gmail.com>
2021-03-02 17:08:21 -0300

    Add Example 3

--- Selenium/example03.py
+++ Selenium/example03.py
@@ -7,10 +7,22 @@ class Exemplo(unittest.TestCase):
     ## Setup
     def setUp(self):
         self.driver = webdriver.Chrome()
-        self.driver.get("http://www.google.com")
+        self.driver.get("http://automationpractice.com/index.php")
 
-    def test_abrir_pagina(self):
-        
+    def test_produto_existente(self):
+        search_field = self.driver.find_element_by_id("search_query_top")
+        search_button = self.driver.find_element_by_name("submit_search")
+        search_field.send_keys('Blouse')
+        search_button.send_keys(Keys.RETURN)
+        item = self.driver.find_element_by_class_name('product_img_link')
+        self.assertTrue(item.is_displayed())
+    
+    def test_produto_nao_existente(self):
+        search_field = self.driver.find_element_by_id("search_query_top")
+        search_button = self.driver.find_element_by_name("submit_search")
+        search_field.send_keys('Blusa')
+        search_button.send_keys(Keys.RETURN)
+        self.assertTrue('No results were found for your search' in self.driver.page_source)
 
     ## TearDown
     def tearDown(self):
